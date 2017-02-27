7a2191f63ef39f82cf16d774994f3b024cc15bd4
billyscears <billyscears@users.noreply.github.com>
2017-02-26 23:58:56 -0500

    Add files via upload
    Assignment 3 Added

---
+++ Assignment 3/CheckerBoard/CheckerBoard/Scripts/Home.js
@@ -0,0 +1,63 @@
+﻿var LastElement = {}
+var CurrentElement = {};
+
+function ToggleHighlight()
+{
+    var current = $(CurrentElement);
+    console.log(current);
+    var last = $(LastElement);
+    console.log(last);
+
+
+    if ($(current).hasClass("highlight piece red") && $(last).hasClass("highlight piece red"))
+    {
+        current.prop("class", "cell piece red");
+    }
+    else if ($(current).hasClass("cell piece red") && !$(last).hasClass("highlight piece black"))
+    {
+        last.prop("class", "cell");
+        current.prop("class", "highlight piece red");
+    }
+    else if ($(current).hasClass("cell") && $(last).hasClass("highlight piece red"))
+    {
+        last.prop("class", "cell");
+        current.prop("class", "cell piece red");
+    }
+    else if ($(current).hasClass("highlight piece black") && $(last).hasClass("highlight piece black"))
+    {
+        current.prop("class", "cell piece black");
+    }
+    else if ($(current).hasClass("cell piece black") && !$(last).hasClass("highlight piece red"))
+    {
+        current.prop("class", "highlight piece black");
+    }
+    else if (($(current).hasClass("cell") || $(current).hasClass("cell piece red")) && $(last).hasClass("highlight piece black")) {
+        last.prop("class", "cell");
+        current.prop("class", "cell piece black");
+    }
+}
+
+
+$(document).ready(function ()
+{
+    var cells = $(".cell");
+    var colorCount = 0;
+
+    for (var i = 0; i < cells.length; i++)
+    {
+        var cell = $(cells[i]);
+        var isDark = colorCount % 2 == 0;
+        var isNextRow = (i + 1) % 8 == 0;
+        colorCount += isNextRow ? 2 : 1;
+        cell.css("background-color", isDark ? "navy" : "white");
+    }
+
+
+    $(".cell").click(function () {
+        CurrentElement = this;
+        console.log(CurrentElement);
+        ToggleHighlight();
+        LastElement = this;
+        console.log(LastElement);
+    });
+});
