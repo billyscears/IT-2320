3598f433a64d0dd0c68e03078bb99cb4a52f8ad3
billyscears <billyscears@users.noreply.github.com>
2017-05-09 20:53:54 -0400

    Uploaded Final

---
+++ Final Project Final/Home.js
@@ -0,0 +1,13 @@
+﻿
+$(document).ready(function () 
+{
+    $("#add").click(function()
+    {
+        $("div.acctInfoLowerCenter").append("<input type="text" name="element2" id="element2" required><br> <input type="submit" id="add" value="Add" class="button">");
+    });
+
+});
+
+$("#login").click(function(){
+    $("p").replaceWith("Hello world!");
+});
